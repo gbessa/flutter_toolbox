9e90113cc8ba934c21b37c9bf84a811defa83568
Gustavo Bessa <gbvirtual@gmail.com>
2019-08-07 11:31:23 +0100

    sync

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Reading and Writing Files',
+      home: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<MyApp> {
+  String data;
+
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    print(directory.path);
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/counter.txt');
+  }
+
+  Future<String> readContent() async {
+    try {
+      final file = await _localFile;
+      // Read the file
+      String contents = await file.readAsString();
+      // Returning the contents of the file
+      return contents;
+    } catch (e) {
+      // If encountering an error, return
+      return 'Error!';
+    }
+  }
+
+  Future<File> writeContent() async {
+    final file = await _localFile;
+    // Write the file
+    return file.writeAsString('This text was written and read from ' + file.path + '/counter.txt');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    writeContent();
+    readContent().then((String value) {
+      setState(() {
+        data = value;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Reading and Writing data')),
+      body: Center(
+        child: Text(
+          'Data read from a file: \n $data',
+        ),
+      ),
+    );
+  }
+}
