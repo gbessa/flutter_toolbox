a06ff4f2ca335f6e8c370415de9f2aa9a6d93944
Gustavo Bessa <gbvirtual@gmail.com>
2019-08-20 10:46:27 +0100

    Save

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_toolbox/widgets/custom_container.dart';
 import 'package:flutter_toolbox/widgets/new_widget.dart';
 import 'package:flutter_toolbox/widgets/screen_widget.dart';
 import 'package:flutter_toolbox/widgets/search_bar.dart';
@@ -11,58 +10,56 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   Widget _buildCard() {
-    return CustomContainer(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.vertical(bottom: Radius.circular(30)),
-      ),
-      children: <Widget>[
-        SizedBox(height: 117),
-        Padding(
-            padding: EdgeInsets.symmetric(horizontal: 28),
-            child: Text(
-              "Welcome to the\nFlutter Tools",
-              style: TextStyle(
-                fontSize: 30,
-                height: 0.9,
-                fontWeight: FontWeight.w900,
-              ),
-            )),
-        SizedBox(height: 40),
-        SearchBar(),
-        SizedBox(height: 42),
-        FloatingActionButton.extended(
-          heroTag: "search",
-          onPressed: search(),
-          label: Text('Search'),
-          icon: Icon(Icons.search),
-          backgroundColor: Colors.blueAccent,
-        ),
-        SizedBox(height: 8.0),
-        FloatingActionButton.extended(
-          heroTag: "new_widget",
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => NewWidget()),
-            );
-          },
-          label: Text('New Widget'),
-          backgroundColor: Colors.blueGrey,
-        ),
-        SizedBox(height: 8.0),
-        FloatingActionButton.extended(
-          heroTag: "screen_widget",
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => ScreenWidget()),
-            );
-          },
-          label: Text('Screen Widget'),
-          backgroundColor: Colors.blueGrey,
-        )
-      ],
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          SizedBox(height: 30),
+          Padding(
+              padding: EdgeInsets.symmetric(horizontal: 28),
+              child: Text(
+                "Welcome to the\nFlutter Tools",
+                style: TextStyle(
+                  fontSize: 30,
+                  height: 0.9,
+                  fontWeight: FontWeight.w900,
+                ),
+              )),
+          SizedBox(height: 8.0),
+          SearchBar(),
+          SizedBox(height: 8.0),
+          RaisedButton(
+            onPressed: search(),
+            child: Text('Search'),
+            color: Colors.blueGrey,
+            splashColor: Colors.blue,
+          ),
+          SizedBox(height: 8.0),
+          RaisedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => NewWidget()),
+              );
+            },
+            child: Text('New Widget'),
+            color: Colors.blueAccent,
+            splashColor: Colors.blue,
+          ),
+          RaisedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ScreenWidget()),
+              );
+            },
+            child: Text('Screed Widget'),
+            color: Colors.blueAccent,
+            splashColor: Colors.blue,
+          ),
+          SizedBox(height: 8.0),
+        ],
+      )
     );
   }
 
@@ -75,6 +72,12 @@ class _HomeState extends State<Home> {
       body: Center(
         child: _buildCard(),
       ),
+      floatingActionButton : FloatingActionButton.extended(
+          heroTag: "add", // Serve para usar mais de 1 floatingActionButton na tela
+          label: Text('Add'),
+          icon: Icon(Icons.add),
+          backgroundColor: Colors.blueAccent, onPressed: () {},
+        )
     );
   }
 
---
+++ lib/widgets/widget_week.dart
@@ -0,0 +1,28 @@
+// 1. SafeArea()
+//    It reads the screen via mediaquery and put the contents in a safe area, to avoid for example the notch area.abstract
+
+// 2. Expanded()
+//    Makes the inner widget to expand and fill the blank space
+
+// 3. Wrap()
+//    Expands itself to better show the inner widgets. It makes itself dinamic and can haev contraints.
+
+// 4. AnimatedContainer()
+//    It provides an implicit animation. Easy peasy. Try the change the color in a setState for example.abstract
+//    Here is the UI diferencial for the app
+
+// 5. Opacity()
+//    It usefull to hide the inner widgets without losing the area.
+//    There is the AnimatedOpacity() too, for gradual changes.
+
+// 6. FutureBuilder()
+//    It's used to build widgets when Dart Futures are envolved.
+//    It's a good practice, for example, to wait for the future, to build the widget.
+//    Check the possible errors too.
+
+// 7. FadeTransition()
+//    
+
+// 8. FloatingActionButton() - FAB
+//    Put it inside the Scaffold
+
