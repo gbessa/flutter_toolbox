1235d7b8a188b4fd80306b1c7caf291fb4426331
Gustavo Bessa <gbvirtual@gmail.com>
2019-08-23 11:10:54 +0100

    some widgets of the week

--- lib/widgets/widget_week.dart
+++ lib/widgets/widget_week.dart
@@ -26,3 +26,24 @@
 // 8. FloatingActionButton() - FAB
 //    Put it inside the Scaffold
 
+// 11. SliverAppBar
+//    To customize the AppBar when do the scroll
+
+// 12. SliverList and SliverGrid
+//    Didn't understand so well yet
+
+// 13. FadeInImage
+//    Widget that leads with the image positioning and appearing (Very nice)
+
+// 14. StreamBuilder
+//    Makes the widget content to be responsive to the Dart Streams.
+
+// 15. InhereritedWidget and InheritedModel
+//    Good for communication between widgets in a tree with no direct inherence, instead of making several jumps for communicate.
+
+// 16. ClipRRect (and ClipOval/ClipPath)
+//    The flutter pattern for borders rounding
+
+// 17. Hero
+//    Hero transitions are a pattern in UI. This is the flutter solution.
+//    Its for exmaple, make an image as a hero in a page transition
